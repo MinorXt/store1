7b9e1515538c2ec4970affd848fdc4c314f845be
student <ilyushkin.vlad@mail.ru>
2024-12-11 08:20:31 +0700

    2

--- handler/add_product.php
+++ handler/add_product.php
@@ -1,10 +1,8 @@
 <?php
-$pdo = require $_SERVER['DOCUMENT_ROOT'].'/store1/db.php';
-///store
+$pdo = require $_SERVER['DOCUMENT_ROOT'].'/store1/db.php'
 
 $name = $_POST['name'];
 $price = $_POST['price'];
 $article = $_POST['article'];
 $pdo->query("insert into product (name, price, article) values ('$name', '$price', '$article')");
 header("Location: /store1/index.php");
-
